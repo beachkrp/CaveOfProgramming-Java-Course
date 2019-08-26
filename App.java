f25e82fe70c9d50a9f39ff40e80c2178eee15b51
Kevin Pearson <krp@cswnet.com>
2019-08-26 08:47:30 -0500

    added move  method to Robot class

--- App.java
+++ App.java
@@ -5,11 +5,16 @@ class Robot{
 	public void speak(String text) {
 		System.out.println(text);
 	}
+	// Creating another method for Robot class
 	  public void jump(int height) {
 		  System.out.println("Jumping: "+ height);
-		  
-	  
+		    
 	}
+	  //Adding anothing method with multiple parameters
+	  public void move (String direction, double distance) {
+		  System.out.println("Moving: "+ distance + " in direction " + direction);
+		  
+	  }
 	
 }
 
@@ -19,8 +24,12 @@ public class App {
 		
 	//Creating a Robo instance
 		Robot sam = new Robot();
+		// calling methods for robot class
 		sam.speak("Hi, I'm Sam");
+		// Demonstrating Jump method
 		sam.jump(7);
+		//Demonstrating move method
+		sam.move("West", 12.2);
 		
 	}
 
