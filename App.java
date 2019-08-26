04e623d27cb8b4ae12cc79fcd6aa0ad51f0c944e
Kevin Pearson <krp@cswnet.com>
2019-08-26 08:36:13 -0500

    initial set up of lesson

---
+++ App.java
@@ -0,0 +1,20 @@
+//New Class Robot
+class Robot{
+	
+	// creating a method for demonstration
+	public void speak() {
+		System.out.println("Hello!");
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	//Creating a Robo instance
+		Robot sam = new Robot();
+		sam.speak();
+		
+	}
+
+}
