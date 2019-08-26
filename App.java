8269cc0208640971d02aa28a28c141b7c4c3c116
Kevin Pearson <krp@cswnet.com>
2019-08-26 09:32:54 -0500

    intial set up of lesson

--- App.java
+++ App.java
@@ -1,36 +1,28 @@
-//New Class Robot
-class Robot{
-	
-	// creating a method for demonstration
-	public void speak(String text) {
-		System.out.println(text);
+class Frog {
+
+	String name;
+	int age;
+	// Create getters for example
+	public String getName() {
+		return name;
 	}
-	// Creating another method for Robot class
-	  public void jump(int height) {
-		  System.out.println("Jumping: "+ height);
-		    
+
+	public int getAge() {
+		return age;
 	}
-	  //Adding anothing method with multiple parameters
-	  public void move (String direction, double distance) {
-		  System.out.println("Moving: "+ distance + " in direction " + direction);
-		  
-	  }
-	
+
 }
 
 public class App {
 
 	public static void main(String[] args) {
-		
-	//Creating a Robo instance
-		Robot sam = new Robot();
-		// calling methods for robot class
-		sam.speak("Hi, I'm Sam");
-		// Demonstrating Jump method
-		sam.jump(7);
-		//Demonstrating move method
-		sam.move("West", 12.2);
-		
+		// initiaze Frog
+		Frog wb = new Frog();
+		wb.age = 1;
+		wb.name = "Bertie";
+
+		System.out.println(wb.getName());
+
 	}
 
 }
