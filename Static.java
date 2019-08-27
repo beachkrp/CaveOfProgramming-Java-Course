3adf3b93fc11ea10b372f0a0412318165b12c83a
Kevin Pearson <krp@cswnet.com>
2019-08-26 21:22:07 -0500

    added showName method

--- Static.java
+++ Static.java
@@ -3,6 +3,10 @@
 class Thing{
 	public String name;
 	public static String description;
+	
+	public void showName() {
+		System.out.println(name);
+	}
 }
 
 public class Static {
@@ -28,6 +32,13 @@ public class Static {
 	System.out.println(thing2.name);
 	
 	System.out.println(thing1.description);
+	
+	//show names
+	 
+	thing1.showName();
+	thing2.showName();
+	
+	
 	}
 	
 
