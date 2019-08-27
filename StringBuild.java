f272690986a621a2fc22616060b9b1724f7c631d
Kevin Pearson <krp@cswnet.com>
2019-08-27 12:45:29 -0500

    Demostrates StringBuilder

--- StringBuild.java
+++ StringBuild.java
@@ -1,21 +1,26 @@
-
-public class StringBuilder {
+public class StringBuild {
 
 	public static void main(String[] args) {
-		
-		//Intialize String
-		String info = "";
-		
-		// Build onto string
-		
-		info += "My Name is Bob";
-		info += " ";
-		info += " I am a builder";
-		
-		System.out.println(info);
-				
-				
-		
-	}
+		// TODO Auto-generated method stub
+			//Intialize String
+			String info = "";
+			
+			// Build onto string
+			
+			info += "My Name is Bob";
+			info += " ";
+			info += " I am a builder";
+			
+			System.out.println(info);
 
+			//String builder 
+			StringBuilder sb =  new StringBuilder();
+			//Append text 
+			sb.append("My Name is Sue.");
+			sb.append(" ");
+			sb.append("I am a Lion tamer.");
+			
+			//output
+			System.out.println(sb.toString());
+		}
 }
