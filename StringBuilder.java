de6b44393c76eccae8666991d59f3d99f5a40d41
Kevin Pearson <krp@cswnet.com>
2019-08-27 12:32:13 -0500

    setting up initial concept

---
+++ StringBuilder.java
@@ -0,0 +1,21 @@
+
+public class StringBuilder {
+
+	public static void main(String[] args) {
+		
+		//Intialize String
+		String info = "";
+		
+		// Build onto string
+		
+		info += "My Name is Bob";
+		info += " ";
+		info += " I am a builder";
+		
+		System.out.println(info);
+				
+				
+		
+	}
+
+}
