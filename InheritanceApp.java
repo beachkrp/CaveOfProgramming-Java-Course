34abe35192a5161c133920cec787b91a1fa67492
Kevin Pearson <krp@cswnet.com>
2019-08-27 19:34:28 -0500

    Initial class declarations

---
+++ InheritanceApp.java
@@ -0,0 +1,16 @@
+
+public class InheritanceApp {
+
+	public static void main(String[] args) {
+		//Creating a Machine instance.
+		Machine mach1 = new Machine();
+		
+		mach1.start();
+		mach1.stop();
+		
+		
+		
+
+	}
+
+}
---
+++ Machine.java
@@ -0,0 +1,13 @@
+
+public class Machine {
+
+	public void start()
+	{
+		System.out.println("Machine started");
+	}
+	
+	public void stop(){
+		System.out.println("Machine Stopped");
+		
+	}
+}
