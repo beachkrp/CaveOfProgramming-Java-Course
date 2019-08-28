67ea6beafe5c57007601645b6545ab857d0f91fc
Kevin Pearson <krp@cswnet.com>
2019-08-27 20:13:00 -0500

    creating classes in package ocean

---
+++ Packages.java
@@ -0,0 +1,12 @@
+import ocean.Fish;
+import ocean.Seaweed;
+
+public class Packages {
+
+	public static void main(String[] args) {
+		
+		Fish fish = new Fish();
+		Seaweed weed = new Seaweed();
+	}
+
+}
