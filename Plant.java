e342e3dfc520e8c75027d7c77b6ca4512b89b1ca
Kevin Pearson <krp@cswnet.com>
2019-08-30 08:31:46 -0500

    demonstrating public variables

---
+++ Plant.java
@@ -0,0 +1,11 @@
+
+public class Plant {
+	public String name;
+	
+	// Create a constructor
+	
+	public Plant() {
+		name = "Ficus";
+	}
+	
+}
---
+++ PublicPrivateProtected.java
@@ -0,0 +1,11 @@
+
+public class PublicPrivateProtected {
+	
+	public static void main(String[] args) {
+		//Create a Plant object 
+		Plant plant = new Plant();
+		
+		System.out.println(plant.name);
+				
+	}
+}
