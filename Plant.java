58156da2a1270a8dd34d122ed468040a9921b873
Kevin Pearson <krp@cswnet.com>
2019-08-30 08:34:20 -0500

    Adding Static final variable

--- Plant.java
+++ Plant.java
@@ -1,6 +1,12 @@
 
 public class Plant {
+	//Bad practice
+	//Declare instance variable
 	public String name;
+	
+	//Acceptable practice
+	//Static Instance variable
+	public final static int ID = 8;
 	
 	// Create a constructor
 	
--- PublicPrivateProtected.java
+++ PublicPrivateProtected.java
@@ -4,8 +4,9 @@ public class PublicPrivateProtected {
 	public static void main(String[] args) {
 		//Create a Plant object 
 		Plant plant = new Plant();
-		
-		System.out.println(plant.name);
+		//Output
+		System.out.println(	plant.name);
+		System.out.println(plant.ID);
 				
 	}
 }
