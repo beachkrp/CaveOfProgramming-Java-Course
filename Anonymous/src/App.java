6279b12965d668d8350ade14655f42bcc48ce44d
Kevin Pearson <krp@cswnet.com>
2019-09-02 12:08:52 -0500

    Set up of classes

---
+++ Anonymous/src/App.java
@@ -0,0 +1,21 @@
+class Machine{
+	public void start() {
+		
+		System.out.println("Starting machine....");
+		
+	}
+	
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		//Create a class instance
+		Machine machine1 = new Machine();
+		//
+		machine1.start();
+		
+
+	}
+
+}
