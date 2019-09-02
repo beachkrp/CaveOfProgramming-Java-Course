5f51e56eba8823b481e23bc0f36166bf2cde530d
Kevin Pearson <krp@cswnet.com>
2019-09-02 12:18:07 -0500

    Adding comments

--- Anonymous/src/App.java
+++ Anonymous/src/App.java
@@ -1,13 +1,16 @@
+//Create parent class
 class Machine{
+	//creating class method
 	public void start() {
-		
+		//Output
 		System.out.println("Starting machine....");
 		
 	}
 	
 }
-
+//Create interface
 interface Plant {
+	//Declaring method
 	public void grow();
 }
 
@@ -18,13 +21,13 @@ public class App {
 		Machine machine1 = new Machine() {
 			@Override
 			public void start() {
-				
+				//Output
 				System.out.println("Camera starting");
 			}
 		};
 		//
 		machine1.start();
-		
+		//Anonymous class from interface
 		Plant plant1 = new Plant() {
 			public void grow() {
 				System.out.println("Growing......");
