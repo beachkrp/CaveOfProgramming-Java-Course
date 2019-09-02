8516aa6361816e8784f82e24759bb6b81a21a09d
Kevin Pearson <krp@cswnet.com>
2019-09-02 11:11:51 -0500

    Array lists works without wild cards

--- Wildcards/src/App.java
+++ Wildcards/src/App.java
@@ -1,41 +1,52 @@
 import java.util.ArrayList;
+
 // Create class
 class Machine {
-
+	// Create a method
 	@Override
 	public String toString() {
 		return "I am a Machine";
 	}
-	
-	
+
 }
-//Create child class
+
+
+// Create child class
 class Camera extends Machine {
-	
-	
-}
 
-
-public class App {
+	// Override an inherited method
 	@Override
 	public String toString() {
-		return "App [toString()=" + super.toString() + "]";
+		return "I am a Camera";
+
 	}
+}
+
+public class App {
+
 	public static void main(String[] args) {
 	//Create a generic variable
-		ArrayList<Machine> list = new ArrayList();
+		ArrayList<Machine> list1 = new ArrayList();
 		
 		//populate the Arraylist
-		list.add(new Machine());
-		list.add(new Machine());
+		list1.add(new Machine());
+		list1.add(new Machine());
 		
+		//Create a generic variable
+		ArrayList<Machine> list2 = new ArrayList();
+				
+				//populate the Arraylist
+		list2.add(new Camera());
+		list2.add(new Camera());
+
 		//show list contents
-		showList(list); 
+		showList(list1);
+		showList(list2);
 		
 		}
 	//Declare another method
-	public static void showList(ArrayList<Machine> list) {
-		for(Machine value: list) {
+	public static void showList(ArrayList<Machine> list0) {
+		for(Machine value: list0) {
 			//output
 			System.out.println(value);
 		}
