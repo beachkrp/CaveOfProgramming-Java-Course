582960451d1dfa15e3a6dbf4b5725c333d9bbe8e
Kevin Pearson <krp@cswnet.com>
2019-09-10 00:01:33 -0500

    Creating a new class

---
+++ INnerClasses/src/App.java
@@ -0,0 +1,12 @@
+	
+public  class App {
+
+	public static void main(String[] args) {
+		
+		Robot robot = new Robot(34);
+		robot.start();
+		
+
+	}
+
+}
---
+++ INnerClasses/src/Robot.java
@@ -0,0 +1,16 @@
+
+public class Robot {
+
+	private int id;
+//Declare a constructor
+	public Robot(int id) {
+		
+		this.id = id;
+	}
+	
+	public void start() {
+		System.out.println("Starting  robot " + id);
+	}
+	
+	
+}
