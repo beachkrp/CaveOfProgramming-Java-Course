c2614f579a50ce6879697744ade9c09a83b2c60b
Kevin Pearson <krp@cswnet.com>
2019-09-12 01:08:52 -0500

    setup of of origial switch assignment

---
+++ Enum/src/App.java
@@ -0,0 +1,30 @@
+
+public class App {
+	
+	public static final int DOG = 0;
+	public static final int CAT = 1;
+	public static final int MOUSE = 2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int animal = CAT;
+		
+		switch (animal) {
+		
+		case DOG:
+			System.out.println("DOG");
+			break;
+
+		case CAT:
+			System.out.println("CAT");
+			break;
+
+		case MOUSE:
+			System.out.println("MOUSE");
+			break;
+		
+		}
+	}
+
+}
